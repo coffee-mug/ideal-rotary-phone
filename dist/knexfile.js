efbe2206838deab67ddc1d2f2c17bfc02144d254
cofee-mug <lucas.kostka@escdijon.eu>
2016-10-19 23:36:59 +0200

    Fixing missing pg package

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 
 module.exports = {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
         database: 'postgres://vadfsvparnklpa:b19YtNTQjjKn-nS-H0WsOxAzKh@ec2-54-243-54-21.compute-1.amazonaws.com:5432/dagipcrs9d77ah',
         user: 'lucas',
