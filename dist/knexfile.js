47da244863c81c8403da24cd77b36f3ab1cee1cf
cofee-mug <lucas.kostka@escdijon.eu>
2016-10-19 23:20:06 +0200

    Trying to replicate server on dist for deployment to heroku

---
+++ dist/knexfile.js
@@ -0,0 +1,10 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'postgresql',
+    connection: {
+        database: 'postgres://vadfsvparnklpa:b19YtNTQjjKn-nS-H0WsOxAzKh@ec2-54-243-54-21.compute-1.amazonaws.com:5432/dagipcrs9d77ah',
+        user: 'lucas',
+        password: 'Pp97#nyGv'
+    } 
+};
--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
+var signupRoute = require('./routes/signup');
+
 app = express();
 app.use(serveStatic(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var port = process.env.PORT || 3000;
+
+// Routes
+app.use('/signup', signupRoute);
+
 app.listen(port);
+
 console.log('server started '+ port);
