4f13fd7ec6f37cac40caee0fd9bef64a91915e84
cofee-mug <lucas.kostka@escdijon.eu>
2016-10-19 23:59:20 +0200

    removed ; that caused heroku error

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -4,4 +4,4 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://vadfsvparnklpa:b19YtNTQjjKn-nS-H0WsOxAzKh@ec2-54-243-54-21.compute-1.amazonaws.com:5433/dagipcrs9d77ah'
     } 
-};
+}
