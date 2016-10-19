113372a2585e28930a07294789c64890dadaee17
cofee-mug <lucas.kostka@escdijon.eu>
2016-10-20 00:43:33 +0200

    Trying to change port of heroku DATABASE_URL

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     client: 'pg',
-    connection: 'postgres://vadfsvparnklpa:b19YtNTQjjKn-nS-H0WsOxAzKh@ec2-54-243-54-21.compute-1.amazonaws.com:5433/dagipcrs9d77ah'
+    connection: 'postgres://vadfsvparnklpa:b19YtNTQjjKn-nS-H0WsOxAzKh@ec2-54-243-54-21.compute-1.amazonaws.com:5432/dagipcrs9d77ah'
 }
