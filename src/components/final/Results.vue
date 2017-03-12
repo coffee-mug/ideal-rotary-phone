2232187b1d9174e503e1f5036c5625ed551b91a3
Anthony Froissant <anthony.froissant@outlook.com>
2017-03-12 23:28:39 +0100

    Ajout reviews + styling

--- src/components/final/Results.vue
+++ src/components/final/Results.vue
@@ -1,29 +1,25 @@
 <template>
-<div class="results-container">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="results-title">Salons / Coiffeurs disponibles</div>
+  <div class="results-container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="results-title">Salons / Coiffeurs disponibles</div>
+          </div>
         </div>
-      </div>
-      <div class="row results">
-        <div class="col-md-6 results-cards">
-          <Card v-for="o in hairdressers" :hairdresser="o" class="row"></Card>
-        </div>
-        <div class="col-md-6 results-map">
-          <gmap-map
-            :center="{lat: 48.862840, lng: 2.333474 }"
-            :zoom="9"
-            class="map">
-            <gmap-marker v-for="h in hairdressers" 
-                        :position="h.geoloc"></gmap-marker>
-          </gmap-map>
+        <div class="row results">
+          <div class="col-md-6 results-cards">
+            <Card v-for="o in hairdressers" :hairdresser="o" class="row"></Card>
+          </div>
+          <div class="col-md-6 results-map">
+            <gmap-map :center="{lat: 48.862840, lng: 2.333474 }" :zoom="9" class="map">
+              <gmap-marker v-for="h in hairdressers" :position="h.geoloc"></gmap-marker>
+            </gmap-map>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <style>
@@ -75,78 +71,87 @@
 
 
 <script>
-import Card from '../funnel/Card.vue';
-import Search from '../final/Search.vue';
-export default {
-  data() {
-    return {
-      results: [],
-      hairdressers: [
-        {
-          name: "Henri de treges",
-          cp: "75015",
-          city: "Paris",
-          street: "15, rue de la convention",
-          photo_link: "https://s3.amazonaws.com/tereza-landing/Home/Coiffeur-du-mois_1.png",
-          geoloc: { lat: 48.845433, lng: 2.279132 },
-          tags: ["Coupe Homme", "Coupe Femme", "Barbier"]
-        },
-        {
-          name: "Nathalie Feuillade",
-          cp: "75014",
-          city: "Paris",
-          street: "14, rue henri bocquillon",
-          photo_link: "https://s3.amazonaws.com/tereza-landing/Home/Coiffeur-du-mois_3.png",
-          geoloc: { lat: 48.841485, lng: 2.289596 },
-          tags: ["Brushing", "Lissage", "Coloration", "Mèches", "Boucles", "Afro"]
-        },
-        {
-          name: "Simone Duchamp",
-          cp: "75004",
-          city: "Paris",
-          street: "12, rue du temple",
-          photo_link: "https://s3.amazonaws.com/tereza-landing/Home/Coiffeur-du-mois_2.png",
-          geoloc: { lat: 48.858286, lng: 2.353283},
-          tags: ["Brushing", "Lissage", "Coloration", "Mèches", "Boucles", "Afro"]
-        },
-      ]
+  import Card from '../funnel/Card.vue';
+  import Search from '../final/Search.vue';
+  export default {
+    data() {
+      return {
+        results: [],
+        hairdressers: [
+          {
+            name: "Henri de treges",
+            cp: "75015",
+            city: "Paris",
+            street: "15, rue de la convention",
+            photo_link: "https://s3.amazonaws.com/tereza-landing/Home/Coiffeur-du-mois_1.png",
+            geoloc: { lat: 48.845433, lng: 2.279132 },
+            tags: ["Coupe Homme", "Coupe Femme", "Barbier"],
+            reviews: [
+              { "rating": 5, "comment": "Bonne prestation<br>Un petit salon sympa, rapide et bien place dans le XV" },
+              { "rating": 3, "comment": "Coloration rapide et reussie, chapeau bas !" },
+              { "rating": 5, "comment": "Que de sympathie dans ce petit salon parisien !<br>Vivement recommande !" }
+            ]
+          },
+          {
+            name: "Nathalie Feuillade",
+            cp: "75014",
+            city: "Paris",
+            street: "14, rue henri bocquillon",
+            photo_link: "https://s3.amazonaws.com/tereza-landing/Home/Coiffeur-du-mois_3.png",
+            geoloc: { lat: 48.841485, lng: 2.289596 },
+            tags: ["Brushing", "Lissage", "Coloration", "Mèches", "Boucles", "Afro"],
+            reviews: [
+              { "rating": 1, "comment": "Je n'y reviendrai pas..." },
+            ]
+          },
+          {
+            name: "Simone Duchamp",
+            cp: "75004",
+            city: "Paris",
+            street: "12, rue du temple",
+            photo_link: "https://s3.amazonaws.com/tereza-landing/Home/Coiffeur-du-mois_2.png",
+            geoloc: { lat: 48.858286, lng: 2.353283 },
+            tags: ["Brushing", "Lissage", "Coloration", "Mèches", "Boucles", "Afro"],
+            reviews: []
+          },
+        ]
 
-    }  
-  },
-  created() {
-    // Salons route search by company name OR city, use if if no haircut provided.
-    if (!window.localStorage.getItem('haircut') ) {
-      this.$http.get('/api/salons/' + window.localStorage.getItem('haircut'),
+      }
+    },
+    created() {
+      // Salons route search by company name OR city, use if if no haircut provided.
+      if (!window.localStorage.getItem('haircut')) {
+        this.$http.get('/api/salons/' + window.localStorage.getItem('haircut'),
+          {
+            params: {
+              city: window.localStorage.getItem('city')
+            }
+          }).then((res) => {
+            this.results = res.body;
+            console.log(res);
+
+          }, (err) => {
+            console.log(err);
+          });
+
+      }
+      this.$http.get('/api/haircuts/' + window.localStorage.getItem('haircut'),
         {
           params: {
             city: window.localStorage.getItem('city')
           }
-      }).then( (res) => {
+        }).then((res) => {
           this.results = res.body;
           console.log(res);
 
-      }, (err) => {
+        }, (err) => {
           console.log(err);
-      });
-
+        });
+    },
+    components: {
+      'Card': Card,
+      'Search': Search
     }
-    this.$http.get('/api/haircuts/' + window.localStorage.getItem('haircut'),
-      {
-        params: {
-          city: window.localStorage.getItem('city')
-        }
-    }).then( (res) => {
-        this.results = res.body;
-        console.log(res);
-
-    }, (err) => {
-        console.log(err);
-    });
-  },
-  components: {
-    'Card': Card,
-    'Search': Search
-  } 
-}
+  }
 
 </script>
